a38628a87996517c7fa2644c5d37bf22654150db
David@Nattr <david@nattr.com>
2017-05-08 18:14:28 +0100

    Set up storyboard to have a table view.  Created custom tableview file.

---
+++ WeatherApp/WeatherApp/ForcastViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ForcastViewController.swift
+//  WeatherApp
+//
+//  Created by David on 08/05/2017.
+//  Copyright © 2017 DavidStanton. All rights reserved.
+//
+
+import UIKit
+
+class ForcastViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}

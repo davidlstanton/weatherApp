a4f8dd82415e9f22283576400b2fda34d14cc8b2
David@Nattr <david@nattr.com>
2017-05-08 18:24:36 +0100

    Added forcastviewcell

---
+++ WeatherApp/WeatherApp/ForecastTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  ForecastTableViewCell.swift
+//  WeatherApp
+//
+//  Created by David on 08/05/2017.
+//  Copyright © 2017 DavidStanton. All rights reserved.
+//
+
+import UIKit
+
+class ForecastTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var weatherDescriptionLabel: UILabel!
+    @IBOutlet weak var dayTemp: UILabel!
+    @IBOutlet weak var nightTemp: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func configureWith(viewModel: DayForecastViewModel) {
+        
+    }
+
+}
